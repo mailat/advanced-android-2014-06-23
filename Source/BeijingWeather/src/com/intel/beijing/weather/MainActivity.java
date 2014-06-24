e75721d18b9227b30f8d6dbf926a97d5ec9656ea
Marius Mailat <marius.mailat@appseleration.com>
2014-06-24 15:15:25 +0800

    AntiMalware example and weather on AsyncTask.

--- Source/BeijingWeather/src/com/intel/beijing/weather/MainActivity.java
+++ Source/BeijingWeather/src/com/intel/beijing/weather/MainActivity.java
@@ -1,21 +1,28 @@
 package com.intel.beijing.weather;
 
-import org.apache.http.HttpConnection;
-
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	TextView weatherText;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		}
 	
 	public void getWeatherData (View v)
 	{
@@ -31,12 +38,54 @@ public class MainActivity extends Activity {
 		String city = textCity.getEditableText().toString();
 		String urlWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",en&units=metric";
 		
-		//TODO get data from urlWeather using HttpConnection ....
-		String weatherResult = "";
-		
 		//display the weather response
-		TextView weatherText = (TextView) findViewById(R.id.weatherText);
-		weatherText.setText(weatherResult);
+		weatherText = (TextView) findViewById(R.id.weatherText);
+		new WeatherUpdater().execute(urlWeather);
+	}
+	
+	class WeatherUpdater extends AsyncTask<String, Integer, String> {
+		private ProgressDialog progress;
+
+		@Override
+		protected String doInBackground(String... url) {
+			try {
+				// transporter for our in->out call
+				HttpClient client = new DefaultHttpClient();
+				HttpGet httpget = new HttpGet(url[0]);
+				String response = client.execute(httpget, new BasicResponseHandler());
+
+				return response;
+
+			} catch (Throwable e) {
+				Log.d("Weather", e.getMessage());
+				return "Error on posting" + e.getMessage();
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			progress.dismiss();
+
+			// parse the JSON and get the temperature
+			try {
+				JSONObject jObj = new JSONObject(result);
+				JSONObject jsonObj = jObj.getJSONObject("main");
+				
+				String weatherString = "";
+				weatherString = new Float(jsonObj.getString("temp")).toString();
+				weatherText.setText(weatherString);
+
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		protected void onPreExecute() {
+			progress = ProgressDialog.show(MainActivity.this,
+					"Get the weather", "Please wait ....");
+		}
+
 	}
 	
 	
